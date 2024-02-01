3073637dcb2e3aff53e0951d71692cd836469619
Oliver343 <oliver_343@hotmail.com>
2024-02-01 14:33:51 +0100

    Dynamic scale disable added and custom sizes

--- src/components/modal.js
+++ src/components/modal.js
@@ -3,7 +3,8 @@ import "./modal.css"
 
 const Modal = (props) => {
     // variables here 
-
+    const [tempWidth, setTempWidth] = useState(920)
+    const [tempHeight, setTempHeight] = useState(653)
     
     return (
         // JSX html here
@@ -12,10 +13,20 @@ const Modal = (props) => {
             <div className="overlay"></div>
             <div className={"modalBody modalBody" + (props.darkMode ? "Dark" : "")}>
                 <h2>SETTINGS</h2>
-                <label for="dark" >Dark Mode:</label> <input name="dark" type="checkbox" onChange={(e) => props.darkFunction(e.target.checked)}/>
+                <label for="dark" >Dark Mode:</label> <input name="dark" type="checkbox" defaultChecked="true" onChange={(e) => props.darkFunction(e.target.checked)}/>
+                <br />
+                <label for="size" >Dynamic Sizing:</label> <input name="size" type="checkbox" defaultChecked="true" onChange={(e) => props.sizeFunction(e.target.checked)}/>
+                <br />
+                <label for="sizeW" >Page Width (px):</label>
+                <input disabled={props.dynamicSize} type="number" value={tempWidth}  onChange={(e) => setTempWidth(parseInt(e.target.value))} min={100} name="sizeW" />
+                <br />
+                <label for="sizeH" >Page Height (px):</label>
+                <input disabled={props.dynamicSize} type="number" value={tempHeight}  onChange={(e) => setTempHeight(parseInt(e.target.value))} min={50} name="sizeH" />
+                <br />
+                <button disabled={props.dynamicSize} onClick={() => {props.widthFunction(tempWidth); props.heightFunction(tempHeight); console.log(tempWidth)}}>SET</button>
                 <br />
                 <br />
-                <button className="standardButton closeButton" onClick={() => props.hideFunction(true)}>CLOSE</button>
+                <button className={"closeButton standardButton standardButton" + (props.darkMode ? "Dark" : "")} onClick={() => props.hideFunction(true)}>CLOSE</button>
             </div>
         </div>
         </>
--- src/pages/main.js
+++ src/pages/main.js
@@ -3,11 +3,14 @@ import Modal from "../components/modal"
 
 const Main = () => {
     // variables here 
-
+    console.log("SAUSAGE")
     const [width, setWidth] = useState(window.innerWidth)
     const [height, setHeight] = useState(window.innerHeight)
+    const [customWidth, setCustomWidth] = useState(920)
+    const [customHeight, setcustomHeight] = useState(653)
     const [modalHide, setModalHide] = useState(true)
-    const [darkMode, setDarkMode] = useState(false)
+    const [darkMode, setDarkMode] = useState(true)
+    const [dynamicSize, setDynamicSize] = useState(true)
 
     function handleResize() {
         setWidth(window.innerWidth)
@@ -20,13 +23,22 @@ const Main = () => {
     
     return (
         // JSX html here
-        <div className={"wholePage wholePage" + darkModeConditional} >
+        <div className={"wholePage wholePage" + darkModeConditional} style={dynamicSize ? {} : {height: customHeight, width: customWidth}}>
 
-            <Modal hidden={modalHide} darkMode={darkMode} hideFunction={setModalHide} darkFunction={setDarkMode} />
+            <Modal 
+                hidden={modalHide}
+                darkMode={darkMode}
+                dynamicSize={dynamicSize}
+                hideFunction={setModalHide}
+                darkFunction={setDarkMode}
+                sizeFunction={setDynamicSize} 
+                widthFunction={setCustomWidth} 
+                heightFunction={setcustomHeight} 
+            />
 
             <div className={"headerBody standardBorder standardBorder" + darkModeConditional}> 
                 <h2>Window Scale Demo</h2>
-                <button className="standardButton" onClick={() => setModalHide(false)}>SETTINGS</button>
+                <button className={"standardButton standardButton" + darkModeConditional} onClick={() => setModalHide(false)}>SETTINGS</button>
             </div>
             <div className={"mainBody standardBorder standardBorder" + darkModeConditional}>
                 Width is: {width}
